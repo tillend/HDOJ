c5941ca4d28f64c72e580f1c3f6962976d2fcda8
Lin <weihaolin2008@qq.com>
2016-05-27 15:54:49 +0800

    HDOJ 2000~2099

--- AC_2060/Main_2062.java
+++ AC_2060/Main_2062.java
@@ -6,9 +6,13 @@ public class Main_2062 {
 
 	public static void main(String[] args) {
 	     Scanner in = new Scanner(System.in);
-	     int n = in.nextInt();
-	     while(n-- > 0){
-	    	 
+	     while(in.hasNextInt()){
+		     int n = in.nextInt();
+		     int m = in.nextInt();
+		     int[] a = new int[n+1];
+		     for(int i = 1; i <= n; i++){
+		    	 a[i] = i;
+		     }
 	     }
 
 	}
